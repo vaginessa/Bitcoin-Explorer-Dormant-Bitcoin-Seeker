6993dd72e16d2401f6f79fbc890960c4b8df7775
dellarosamarco <dellarosamarco00@gmail.com>
2022-03-13 12:51:55 +0100

    Stats chart

--- lib/Views/boost/Boost.dart
+++ lib/Views/boost/Boost.dart
@@ -1,4 +1,5 @@
 import 'package:dormant_bitcoin_seeker_flutter/Views/boost/boost_card.dart';
+import 'package:dormant_bitcoin_seeker_flutter/Views/boost/stats_chart.dart';
 import 'package:dormant_bitcoin_seeker_flutter/global.dart';
 import 'package:flutter/material.dart';
 
@@ -16,15 +17,43 @@ class _BoostState extends State<Boost> {
       backgroundColor: backgroundColor,
       body: Container(
         margin: const EdgeInsets.only(top:35),
-        child: Column(
-          children: const [
-            BoostCard(title: "Increase wallet generation speed by 10 for 5 minutes", description: "", actionName: "WATCH AN AD",),
-            BoostCard(title: "Increase boost time for 15 minutes", description: "", actionName: "WATCH AN AD",),
-            BoostCard(title: "Title 2", description: "", actionName: "1.99 \$",),
-            BoostCard(title: "Title 3", description: "", actionName: "3.99 \$",),
+        child: Column(  
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(18, 18, 18, 1)
+              ),
+              child: Container(
+                margin: const EdgeInsets.all(15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Current stats", style: TextStyle(color: Colors.white, fontSize: 27.5),),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        StatsChart(title : "Title 1"),
+                        StatsChart(title : "Title 2")
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: const [
+                BoostCard(title: "Increase wallet generation speed by 10 for 5 minutes", description: "", actionName: "WATCH AN AD",),
+                BoostCard(title: "Increase boost time for 15 minutes", description: "", actionName: "WATCH AN AD",),
+                BoostCard(title: "Title 2", description: "", actionName: "1.99 \$",),
+                BoostCard(title: "Title 3", description: "", actionName: "3.99 \$",),
+              ],
+            )
           ],
         ),
-      ),
+      )
     );
   }
 }
---
+++ lib/Views/boost/stats_chart.dart
@@ -0,0 +1,41 @@
+import 'package:dormant_bitcoin_seeker_flutter/global.dart';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class StatsChart extends StatefulWidget {
+  const StatsChart({ Key? key, required this.title }) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatsChart> createState() => _StatsChartState();
+}
+
+class _StatsChartState extends State<StatsChart> {
+
+  int walletGenerationSpeed = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top:20),
+      child: CircularPercentIndicator(
+        header: Text(widget.title, style: const TextStyle(color: Colors.white, fontSize: 20),),
+        radius: 75.0,
+        lineWidth: 10.0,
+        animation: true,
+        percent: walletGenerationSpeed/100,
+        center: Text(
+          walletGenerationSpeed.toString(),
+          style: const TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w400,
+              color: Colors.white),
+        ),
+        backgroundColor: Colors.grey,
+        circularStrokeCap: CircularStrokeCap.round,
+        progressColor: balanceColor,
+      ),
+    );
+  }
+}
