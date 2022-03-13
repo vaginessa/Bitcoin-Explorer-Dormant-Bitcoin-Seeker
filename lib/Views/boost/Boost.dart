8080f5157c408f2fa7f60d1cbafa9fa040292dc2
dellarosamarco <dellarosamarco00@gmail.com>
2022-03-13 13:00:42 +0100

    Chart update

--- lib/Views/boost/Boost.dart
+++ lib/Views/boost/Boost.dart
@@ -43,13 +43,21 @@ class _BoostState extends State<Boost> {
                 ),
               ),
             ),
-            Column(
-              children: const [
-                BoostCard(title: "Increase wallet generation speed by 10 for 5 minutes", description: "", actionName: "WATCH AN AD",),
-                BoostCard(title: "Increase boost time for 15 minutes", description: "", actionName: "WATCH AN AD",),
-                BoostCard(title: "Title 2", description: "", actionName: "1.99 \$",),
-                BoostCard(title: "Title 3", description: "", actionName: "3.99 \$",),
-              ],
+            Expanded(
+              child: Container(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: const [
+                      BoostCard(title: "Increase wallet generation speed by 10 for 5 minutes", description: "", actionName: "WATCH AN AD",),
+                      BoostCard(title: "Increase boost time for 15 minutes", description: "", actionName: "WATCH AN AD",),
+                      BoostCard(title: "Title 2", description: "", actionName: "1.99 \$",),
+                      BoostCard(title: "Title 3", description: "", actionName: "3.99 \$",),
+                      BoostCard(title: "Title 3", description: "", actionName: "3.99 \$",),
+                      BoostCard(title: "Title 3", description: "", actionName: "3.99 \$",),
+                    ],
+                  ),
+                ),
+              ),
             )
           ],
         ),
