747796d3a93c8f3078a759233da6bc594b7ce807
dellarosamarco <dellarosamarco00@gmail.com>
2022-04-02 14:05:28 +0200

    Update info view

--- lib/Views/info/Info.dart
+++ lib/Views/info/Info.dart
@@ -1,3 +1,4 @@
+import 'package:dormant_bitcoin_seeker_flutter/global.dart';
 import 'package:flutter/material.dart';
 
 class Info extends StatelessWidget {
@@ -5,8 +6,21 @@ class Info extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Info view"),
+    return Container(
+      margin: const EdgeInsets.only(top:60, right:20,left:20),
+      child: Column(
+        children: const [
+          Center(child: Text("Dormant Bitcoin Seeker", style : TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color : Colors.orange))),
+          SizedBox(height:30),
+          Center(child: Text("It has been estimated that there are more than 3 million bitcoins lost forever... or maybe not forever! This app gives you the possibility to search for them by generating random private keys of bitcoin addresses and giving you the balance of those in real time.", style: TextStyle(color : Colors.white, fontSize: 15),textAlign: TextAlign.center,),),
+          SizedBox(height:30),
+          Center(child: Text("There is also the possibility to generate 12 phrase wallets (brainwallets).", style: TextStyle(color : Colors.white, fontSize: 15),textAlign: TextAlign.center,),),
+          SizedBox(height:30),
+          Center(child: Text("You can also search for Bitcoin wallets by their private key, public key, or address (if you search by public key or address, you can't calculate the private key).", style: TextStyle(color : Colors.white, fontSize: 15),textAlign: TextAlign.center,),),
+          SizedBox(height:30),
+          Center(child: Text("Once you have generated a Bitcoin wallet, you can check the balance of it directly from this app, or you can watch it directly from the Bitcoin blockchain.", style: TextStyle(color : Colors.white, fontSize: 15),textAlign: TextAlign.center,),),
+        ],
+      ),
     );
   }
 }
